165d37532a7c5b19a27efcc9ac4a7ad4960cc1cc
Uditha Jayarathne <ujayarat@robarts.ca>
2017-07-18 21:34:55 -0400

    Embedding vtkRenderWindow and vtkOpenVRRenderWindow in QVTKOpenGLWidget

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
-
-
 // Qt includes
 #include <QApplication>
 #include <QHBoxLayout>
 #include <QWidget>
 
 // VTK includes
-// #include <vtkOpenVRRenderWindow.h>
-// #include <vtkRenderWindow.h>
+#include <vtkOpenVRRenderWindow.h>
+#include <vtkRenderWindow.h>
+#include <QVTKOpenGLWidget.h>
 
 int main(int argc, char* argv[])
 {
   QApplication app(argc, argv);
 
   QWidget topLevel;
+  topLevel.setMinimumSize(600, 300);
+
   QHBoxLayout layout(&topLevel);
 
-  // XXX Replace QWidget with QVTKOpenGLWidget + vtkRenderWindow
-  QWidget * openGRViewer = new QWidget(&topLevel);
+  // create QVTKOpenGLWidget + vtkRenderWindow
+  vtkRenderWindow *renWin = vtkRenderWindow::New();
+  QVTKOpenGLWidget *openGRViewer = new QVTKOpenGLWidget(&topLevel);
+  openGRViewer->SetRenderWindow(renWin);
   layout.addWidget(openGRViewer);
 
-  // XXX Replace QWidget with QVTKOpenGLWidget + vtkOpenVRRenderWindow
-  QWidget * openVRViewer = new QWidget(&topLevel);
+  // create QVTKOpenGLWidget + vtkOpenVRRenderWindow
+  vtkOpenVRRenderWindow *vrRenWin = vtkOpenVRRenderWindow::New();
+  QVTKOpenGLWidget *openVRViewer = new QVTKOpenGLWidget(&topLevel);
+  openVRViewer->SetRenderWindow(vrRenWin);  
   layout.addWidget(openVRViewer);
   
   topLevel.show();
